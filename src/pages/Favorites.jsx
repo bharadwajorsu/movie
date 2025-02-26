8e406e477cf47d6063f791efffa3580f130618ab
bharadwajorsu <bharadwajorsu036@gmail.com>
2025-02-26 22:00:20 +0530

    Add files via upload

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,23 @@
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+
+function Favorites() {
+  const { favorites } = useMovieContext();
+
+  if (favorites) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+  
+}
+
+export default Favorites;
